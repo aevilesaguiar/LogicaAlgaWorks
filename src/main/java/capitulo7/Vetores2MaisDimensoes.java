61fe405984c7cce3cdb26d25551363bbab6d7ecd
aevilesaguiar <aeviles@hotmail.com>
2022-07-15 10:51:12 -0300

    finalizando capitulo7

---
+++ src/main/java/capitulo7/Vetores2MaisDimensoes.java
@@ -0,0 +1,38 @@
+package capitulo7;
+
+public class Vetores2MaisDimensoes {
+    public static void main(String[] args) {
+
+        Double faturamentoDia=1000.00;
+
+        Double[] faturamentoJaneiro = new Double[]{1500.00, 2000.00,17000.00};
+        Double[] faturamentoFevereiro = new Double[]{1900.00, 2900.00,12000.00};
+
+        Double[][] faturamentoAnual = new Double[][]{faturamentoJaneiro, faturamentoFevereiro};
+
+        Double[][][] faturamentoQuinquenal = new Double[][][]{faturamentoAnual};
+
+        Double[][][][]faturamentoDecada = new Double[][][][]{faturamentoQuinquenal};
+
+
+    /*   // System.out.println("Faturamento do dia 1 janeiro: "+faturamentoAnual[0][0]);
+
+
+        for (int i = 0; i <faturamentoAnual.length ; i++) {
+
+            System.out.println("Mês: "+ (i+1));
+
+            //          Double[] mes= faturamentoAnual[i];  é a mesma coisa que utilizar  Double dia=faturamentoAnual[i][j];
+
+            for (int j = 0; j <faturamentoAnual.length ; j++) {
+
+                Double dia=faturamentoAnual[i][j];
+
+                System.out.println("Dia "+(j+1)+" : "+dia);
+            }
+
+        }*/
+
+
+    }
+}
